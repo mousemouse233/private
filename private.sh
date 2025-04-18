314740fcb90f04ccbe9e99e200cb6ff4ffd57341
mousemouse233 <aa316191@outlook.com>
2025-04-18 08:39:21 +0800

    Create private.sh

---
+++ private.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apt-get update
+
+sudo apt install git -y
+
+export GITHUB_TOKEN="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"
+
+git clone https://$GITHUB_TOKEN@github.com/lisi-123/private.git
+
+sudo chmod +x /root/private/setup.sh && sudo /root/private/setup.sh
