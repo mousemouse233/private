cb5d6c2dee8934d2f8b2ab7fd982d09074ab1ed7
mousemouse233 <aa316191@outlook.com>
2025-04-23 15:43:05 +0800

    Update private.sh

--- private.sh
+++ private.sh
@@ -6,7 +6,10 @@ sudo apt install git -y
 
 export GITHUB_TOKEN="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"
 
-# 将 $GITHUB_TOKEN 插入到 URL 中
-git clone https://$GITHUB_TOKEN@github.com/lisi-123/private.git
+# 设置 git 配置以使用 token 作为用户名密码
+git config --global credential.helper store
+echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" > ~/.git-credentials
+
+git clone https://github.com/lisi-123/private.git
 
 sudo chmod +x /root/private/setup.sh && sudo /root/private/setup.sh
