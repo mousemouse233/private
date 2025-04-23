b6f347672f7fc21fcac3b494fd4375b2a4d472ea
mousemouse233 <aa316191@outlook.com>
2025-04-23 15:38:20 +0800

    Update private.sh

--- private.sh
+++ private.sh
@@ -6,7 +6,7 @@ sudo apt install git -y
 
 export GITHUB_TOKEN="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"
 
+# 将 $GITHUB_TOKEN 插入到 URL 中
 git clone https://$GITHUB_TOKEN@github.com/lisi-123/private.git
 
 sudo chmod +x /root/private/setup.sh && sudo /root/private/setup.sh
-
