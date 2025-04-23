1372873ce37f8e19ef1eb394cabb788e05effade
mousemouse233 <aa316191@outlook.com>
2025-04-23 16:39:23 +0800

    Update private.sh

--- private.sh
+++ private.sh
@@ -4,15 +4,6 @@ apt-get update
 
 sudo apt install git -y
 
-export GITHUB_TOKEN="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"
-
-# 设置 Git 凭证助手
-git config --global credential.helper store
-
-# 通过令牌设置 Git 凭证
-echo "https://$GITHUB_TOKEN:x-oauth-basic@github.com" > ~/.git-credentials
-
-# 克隆仓库
 git clone https://github.com/lisi-123/private.git
 
 sudo chmod +x /root/private/setup.sh && sudo /root/private/setup.sh
